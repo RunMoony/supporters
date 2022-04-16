9b55c3e07641fb58674a7930ed84685ee29e8a84
KwangJe Moon <iqboy963295@gmail.com>
2022-04-16 13:02:23 +0900

    feat : Enter page

--- components/input.tsx
+++ components/input.tsx
@@ -1,36 +1,31 @@
 import type { UseFormRegisterReturn } from "react-hook-form";
 
 interface InputProps {
-  label: string;
   name: string;
   kind?: "text" | "phone";
   type: string;
-
+  register: UseFormRegisterReturn;
   required: boolean;
 }
 
 export default function Input({
-  label,
   name,
   kind = "text",
   type,
+  register,
   required,
 }: InputProps) {
   return (
     <div>
-      <label
-        className='mb-1 block text-sm font-medium text-white'
-        htmlFor={name}
-      >
-        {label}
-      </label>
       {kind === "text" ? (
         <div className='rounded-md relative flex  items-center shadow-sm'>
           <input
             id={name}
             required={required}
             type={type}
+            {...register}
             className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-600 focus:border-pink-600'
+            placeholder='ex) asdasd@gmail.com'
           />
         </div>
       ) : null}
@@ -43,6 +38,7 @@ export default function Input({
           <input
             id={name}
             required={required}
+            {...register}
             type={type}
             className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-600 focus:border-pink-600'
             placeholder='ex) 8201012345678'
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple": "#37003c",
+      },
+    },
   },
   plugins: [],
 };
