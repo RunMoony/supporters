e9c3b082c148bac21bb6b4b0a9efafc96ccb51dc
KwangJe Moon <iqboy963295@gmail.com>
2022-05-09 21:51:31 +0900

    teamList object

---
+++ data/teamList.ts
@@ -0,0 +1,152 @@
+type teamListTypes = {
+  label: string;
+  leagueId: number;
+  teams: Array<any>;
+}[];
+
+export const teamList: teamListTypes = [
+  {
+    label: "EPL",
+    leagueId: 47,
+    teams: [
+      {
+        teamId: 8456,
+        teamNameLong: "Manchester City",
+        teamName: "MNC",
+        teamColor: "#6CABDD",
+        teamSubColor: "#1C2C5B",
+      },
+      {
+        teamId: 8586,
+        teamNameLong: "Tottenham",
+        teamName: "TOT",
+        teamColor: "#132257",
+        teamSubColor: "#f7f7f7",
+      },
+      {
+        teamId: 8455,
+        teamNameLong: "Chelsea",
+        teamName: "CHE",
+        teamColor: "#034694",
+        teamSubColor: "#6A7AB5",
+      },
+      {
+        teamId: 8650,
+        teamNameLong: "Liverpool",
+        teamName: "LIV",
+        teamColor: "#C8102E",
+        teamSubColor: "#00B2A9",
+      },
+      {
+        teamId: 10260,
+        teamNameLong: "Manchster United",
+        teamName: "MAN",
+        teamColor: "#DA291C",
+        teamSubColor: "#FBE122",
+      },
+
+      {
+        teamId: 8197,
+        teamNameLong: "Leicester City",
+        teamName: "LEI",
+        teamColor: "#003090",
+        teamSubColor: "#FDBE11",
+      },
+
+      {
+        teamId: 9825,
+        teamNameLong: "Arsenal",
+        teamName: "ARS",
+        teamColor: "#EF0107",
+        teamSubColor: "#9C824A",
+      },
+      {
+        teamId: 8654,
+        teamNameLong: "West Ham",
+        teamName: "WHU",
+        teamColor: "#7A263A",
+        teamSubColor: "#F3D459",
+      },
+      {
+        teamId: 8668,
+        teamNameLong: "Brentford",
+        teamName: "BRN",
+        teamColor: "#e30613",
+        teamSubColor: "#f7f7f7",
+      },
+      {
+        teamId: 9817,
+        teamNameLong: "Watford",
+        teamName: "WAT",
+        teamColor: "#11210C",
+        teamSubColor: "#FBEE23",
+      },
+      {
+        teamId: 10204,
+        teamNameLong: "Brighton",
+        teamName: "BHA",
+        teamColor: "#0057B8",
+        teamSubColor: "#FFCD00",
+      },
+
+      {
+        teamId: 10252,
+        teamNameLong: "Aston Villa",
+        teamName: "AVL",
+        teamColor: "#670E36",
+        teamSubColor: "#95BFE5",
+      },
+      {
+        teamId: 8191,
+        teamNameLong: "Burnley",
+        teamName: "BUR",
+        teamColor: "#6C1D45",
+        teamSubColor: "#99D6EA",
+      },
+
+      {
+        teamId: 8602,
+        teamNameLong: "Wolverhamton",
+        teamName: "WOL",
+        teamColor: "#231F20",
+        teamSubColor: "#FDB913",
+      },
+      {
+        teamId: 10261,
+        teamNameLong: "Newcastle United",
+        teamName: "NEW",
+        teamColor: "#241F20",
+        teamSubColor: "#FFFFFF",
+      },
+      {
+        teamId: 8466,
+        teamNameLong: "Southamton",
+        teamName: "SOU",
+        teamColor: "#D71920",
+        teamSubColor: "#130C0E",
+      },
+
+      {
+        teamId: 9826,
+        teamNameLong: "Crystal Palace",
+        teamName: "CRY",
+        teamColor: "#1B458F",
+        teamSubColor: "#A7A5A6",
+      },
+      {
+        teamId: 9850,
+        teamNameLong: "Norwich City",
+        teamName: "NOR",
+        teamColor: "#00A650",
+        teamSubColor: "#FFF200",
+      },
+      {
+        teamId: 8463,
+        teamNameLong: "Leeds United",
+        teamName: "LEE",
+        teamColor: "#1D428A",
+        teamSubColor: "#AC944D",
+      },
+    ],
+  },
+];
